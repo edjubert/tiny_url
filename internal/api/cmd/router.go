5997528ac8828f5d89d637cbfcb17716c3cdd2f4
edjubert <edouard.jubert@proton.me>
2024-11-26 10:48:35 +0100

    [api] add router skeleton

---
+++ internal/api/cmd/router.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/edjubert/tiny-url/internal/api/pkg/handlers/cmd"
+	"github.com/edjubert/tiny-url/internal/pkg/config"
+	"github.com/edjubert/tiny-url/internal/pkg/database"
+)
+
+func Router(conf *config.Configuration, db *database.Database) *gin.Engine {
+	switch conf.Server.Mode {
+	case "release":
+		gin.SetMode(gin.ReleaseMode)
+	case "debug":
+		gin.SetMode(gin.DebugMode)
+	default:
+		gin.SetMode(gin.DebugMode)
+	}
+
+	app := gin.New()
+
+	app.Use(gin.Recovery())
+
+	_ := cmd.New(conf, db)
+
+	return app
+}
